f921398d2facbd73cdad10b8ed0941aac31bd9db
alchain <alchaincyf@gmail.com>
2023-07-11 17:59:31 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: ' ChatGPT精进指南',
+        title: ' AI第一课',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.png',
